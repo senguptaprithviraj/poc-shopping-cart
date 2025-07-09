ae135b39d3e37d83dd1723da557bc541267851d0
senguptaprithviraj <>
2025-07-09 10:02:00 -0400

    updated env

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION,
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
   useCdn: false,
-  token: process.env.SANITY_TOKEN,
+  token: import.meta.env.VITE_SANITY_TOKEN,
 });
